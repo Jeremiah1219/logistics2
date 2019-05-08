fdc70888bcf59d79ad07ca58cc79f49d2ab1c314
Jeremiah <jermiah1219@gmail.com>
2019-05-08 10:58:00 +0800

    rename a file

--- index.php
+++ index.php
@@ -1,1 +1,12 @@
-awdlfjasdfkjas
+git branch newbranch
+git checkout newbranch
+git add . 
+git commit -a -m "new commit"
+git push -u hr1 newbranch
+
+punta kay git para mag merge and pull request after
+balik na kay bash para mag bash
+
+git checkout master
+git pull hr1 master
+git branch -d newbranch
